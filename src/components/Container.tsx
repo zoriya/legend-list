3a2ec427f132e9ba03c6b17af594bbc670c80bc9
Zoe Roux <zoe.roux@zoriya.dev>
2026-02-13 11:52:38 +0100

    Fix gaps on web

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -82,13 +82,15 @@ export const Container = typedMemo(function Container<ItemT>({
             // Create padding styles for both horizontal and vertical layouts with multiple columns
             if (horizontal) {
                 paddingStyles = {
+                    paddingBottom: numColumns > 1 ? (rowGap || gap || 0) / 2 : undefined,
                     paddingRight: columnGap || gap || undefined,
-                    paddingVertical: numColumns > 1 ? (rowGap || gap || 0) / 2 : undefined,
+                    paddingTop: numColumns > 1 ? (rowGap || gap || 0) / 2 : undefined,
                 };
             } else {
                 paddingStyles = {
                     paddingBottom: rowGap || gap || undefined,
-                    paddingHorizontal: numColumns > 1 ? (columnGap || gap || 0) / 2 : undefined,
+                    paddingLeft: numColumns > 1 ? (columnGap || gap || 0) / 2 : undefined,
+                    paddingRight: numColumns > 1 ? (columnGap || gap || 0) / 2 : undefined,
                 };
             }
         }
